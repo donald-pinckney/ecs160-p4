a834af04b1077c98460c3ccfefb0923fbcd3303c
megharjain <mrjain@ucdavis.edu>
2018-03-11 00:47:35 -0800

    Wrote findColumn

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,16 +1,60 @@
 #include <stdio.h>
 
-void maxTweeter(char *filename) {
-
+void maxTweeter(FILE *fp) 
+{
+    
 }
 
-int main(int argc, char *argv[]) {
-	if(argc != 2) {
+int main(int argc, char *argv[]) 
+{
+	if(argc != 2) 
+    {
 		printf("Invalid arguments\n");
 		exit(1);
 	}
 	char *filename = argv[1];
 
+    FILE * file;
+    file = fopen(filename, "r");
+    
 	maxTweeter(filename); 
 }
+
+int findColumn(FILE *fp)
+{
+    
+    char buffer[377], *token;
+    int i;
+    i = 0;
+    
+    if (fgets(buffer , 377, fp) == NULL)
+    {
+        return -1;
+    }
+    
+    
+    token = strtok(buffer, ",");
+    
+    while (token != NULL)
+    {
+        
+        if (strcmp(token, "\"name\"") == 0)
+        {
+            return i;
+        }
+        
+        token = strtok(NULL,",");
+        i++;
+    }
+    
+    return -1;
+    
+}
+
+
+
+
+
+
+
 
